1ef379072575f1f5189a3afad959aa6991dddb03
macmc <macmecy@gmail.com>
2021-10-15 13:51:32 +0200

    adjust

--- src/pages/DetailsAppart/index.jsx
+++ src/pages/DetailsAppart/index.jsx
@@ -1,13 +1,19 @@
 import { Component } from 'react'
 
+import { dataAppart } from '../../data/dataAppart'
+
 class DetailsAppart extends Component {
   render() {
     const { id } = this.props.match.params
-    return (
-      <div>
-        <h1>Appartement : {id}</h1>
-      </div>
-    )
+    const result = dataAppart.filter((el) => {
+      if (el.id === id) {
+        return el
+      } else {
+        return false
+      }
+    })
+
+    return <div>{result[0].id}</div>
   }
 }
 
