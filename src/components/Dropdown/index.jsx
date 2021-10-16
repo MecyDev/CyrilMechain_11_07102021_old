aea57bf81f4bf5218fa217193404844770a61d25
macmc <macmecy@gmail.com>
2021-10-16 15:51:24 +0200

    finish

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -3,10 +3,38 @@ import { Component } from 'react'
 import './dropdown.css'
 
 class Dropdown extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      classNameDropContent: 'close',
+      classNameArrow: '',
+    }
+  }
+
+  handleClick = () => {
+    if (this.state.classNameDropContent === 'close') {
+      this.setState({
+        classNameDropContent: '',
+        classNameArrow: 'rotate',
+      })
+    } else {
+      this.setState({
+        classNameDropContent: 'close',
+        classNameArrow: '',
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="dropdown">
-        <h2>{this.props.title}</h2>
+      <div className="dropdown-container">
+        <div className="dropdown" onClick={this.handleClick}>
+          <h2>{this.props.title}</h2>
+          <span className={`arrow ${this.state.classNameArrow}`}></span>
+        </div>
+        <div className={`dropdown-content ${this.state.classNameDropContent}`}>
+          <p>{this.props.content}</p>
+        </div>
       </div>
     )
   }
